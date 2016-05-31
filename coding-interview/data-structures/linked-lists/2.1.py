0ba1c4b4b394869e8dcdaae79abcc05bb6cfb8e3
abdujabbor <abdujabbor1987@gmail.com>
2016-05-31 19:08:20 +0500

    coding interview learning

--- coding-interview/data-structures/linked-lists/2.1.py
+++ coding-interview/data-structures/linked-lists/2.1.py
@@ -1,51 +1,32 @@
 # remove duplicates from unsorted linked list
 from Node import Node
 
-# method for removing duplicates from linked list
-def remove_duplicates(n):
-    start = n
-    lst = Node(n.value)
-    visited = [n.value]
-    while start.next:
-        if start.value in visited:
-            start = start.next
-        else:
-            visited.append(start.value)
-            lst.add_to_tail(start.value)
-            start = start.next
 
-    return lst
-
-# method for removing duplicate from linked list withoud additional buffer
-def remove_duplicates_without_additional_space(n):
-    start = n
-    while start:
-        v = start.value
-        end = start.next
-        while end and end.next:
-            tmp = end.next
-            found = False
-            while tmp and tmp.value == v:
-                tmp = tmp.next
-                found = True
-            if found:
-                # if tmp and tmp.next:
-                if tmp:
-                    end.next = tmp.next
-                else:
-                    end = None
-            else:
-                end = end.next
-        start = start.next
-    return n
-
-n = Node(1)
-n.add_to_tail(1)
-n.add_to_tail(1)
-n.add_to_tail(1)
-n.add_to_tail(1)
-n.add_to_tail(1)
-
-n = remove_duplicates_without_additional_space(n)
-
-n.print_items()
+# class A:
+#     def __init__(self):
+#         pass
+#     def method(self, a: list):
+#         pass
+#
+# a = A()
+# #
+# def param(lst: list, a: int):
+#     lst.append(a)
+#
+#
+# l = []
+# param(l, 1)
+# print(l)
+#
+#
+# n = Node(1)
+# n.add_to_tail(2)
+# n.add_to_tail(3)
+# n.add_to_tail(4)
+# n.add_to_tail(5)
+# n.add_to_tail(6)
+# n.add_to_tail(7)
+# n.add_to_tail(8)
+#
+# n.remove_middle_element()
+# n.print_items()
--- coding-interview/data-structures/linked-lists/Node.py
+++ coding-interview/data-structures/linked-lists/Node.py
@@ -30,3 +30,60 @@ class Node:
             obj.next = self.del_by_key(obj.next, key - 1)
         return obj
 
+    def remove_duplicates(self):
+        start = self
+        lst = Node(self.value)
+        visited = [self.value]
+        while start:
+            if start.value not in visited:
+                lst.add_to_tail(start.value)
+                visited.append(start.value)
+
+            start = start.next
+        return lst
+
+    def remove_duplicates_without_additional_space(self):
+        start = self
+        while start:
+            v = start.value
+            end = start
+            while end and end.next:
+                while end.next and end.next.value == v:
+                    end.next = end.next.next
+                end = end.next
+            start = start.next
+
+    def find_nth_last_element(self, n):
+        start = self
+        end = self
+        c = 0
+        while end:
+            c += 1
+            end = end.next
+        t = c - n
+        while start and t:
+            t -= 1
+            start = start.next
+
+        if start and t == 0:
+            return start.value
+        else:
+            return -1
+
+    def remove_middle_element(self):
+        c = 0
+        start = self
+        end = self
+        while end.next:
+            c += 1
+            end = end.next
+        m = c // 2
+
+        while start and m > 1:
+            start = start.next
+            m -= 1
+
+        start.next = start.next.next
+
+    def add_linked_lists(self, lst: list):
+        pass
